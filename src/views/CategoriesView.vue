e4a7bb1cce20bf7b44412a6e9f716e0d8a2b03a3
Ahmed Mamdouh <ahmed.banawi123@gmail.com>
2024-07-27 16:10:29 +0300

    finish set pinia stores (#2)

---
+++ src/views/CategoriesView.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>categories</h1>
+
+  <p>
+    {{ getCategories }}
+  </p>
+</template>
+
+<script>
+import { useCategoriesStore } from '@/stores/categories.store'
+import { mapState, mapActions } from 'pinia'
+export default {
+  name: 'CategoriesView',
+  data() {
+    return {
+    }
+  },
+  computed: {
+    ...mapState(useCategoriesStore, ['getCategories'])
+  },
+  async mounted() {
+    await this.getAllCategories()
+  },
+  methods: {
+    ...mapActions(useCategoriesStore, ['getAllCategories'])
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
