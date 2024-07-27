afa33139ab81fd615421decebc5c98ec7d8e5c61
Abderazek Emad <a.razek@gmail.com>
2024-07-27 12:49:27 +0300

    configer I18n & vue-router packages

---
+++ src/i18n/locale/ar/index.js
@@ -0,0 +1,5 @@
+import { default as global } from "./global.json";
+
+export default {
+  ...global,
+};
---
+++ src/views/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <language-switcher />
+    test home
+  </div>
+</template>
+
+<script>
+import LanguageSwitcher from "@/components/LanguageSwitcher.vue";
+export default {
+  components: {
+    LanguageSwitcher,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
