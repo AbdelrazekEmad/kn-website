9e9fde8f5478ce9b41982cdb22d509b66affb5a1
Abderazek Emad <a.razek@gmail.com>
2024-07-31 22:19:34 +0300

    blogs page

--- src/i18n/locale/en/index.js
+++ src/i18n/locale/en/index.js
@@ -1,5 +1,7 @@
 import { default as global } from "./global.json";
+import { default as blog } from "./blog.json";
 
 export default {
   ...global,
+  ...blog,
 };
--- src/views/BlogsView.vue
+++ src/views/BlogsView.vue
@@ -1,18 +1,83 @@
 <template>
-  <h1>blogs</h1>
-
-  <p>
-    {{ getBlogs }}
-  </p>
+  <main-banner :type="'breadcrumb'" :banner-title="$t('BLOG.MAIN_BANNER')" :current-page="$t('GLOBAL.NAVS.BLOG')" :img-url="'https://placehold.co/900x600'" />
+  <section class="my-5">
+    <div class="container">
+      <div class="row g-4">
+        <div class="col-12 col-md-6 col-lg-4" v-for="blog in blogs" :key="blog.id">
+          <div class="blog">
+            <div class="blog__body">
+              <img :src="blog.img" alt="" class="blog__img img-fluid" />
+              <a href="" class="blog__title" :title="blog.name">{{ blog.name }}</a>
+              <p class="blog__description">{{ blog.description }}</p>
+              <a href="" class="blog__read-more">{{ $t("BLOG.READ_MORE") }} <i class="fa-solid fa-angles-right"></i></a>
+            </div>
+            <div class="blog__footer">
+              {{ blog.date }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 import { useBlogsStore } from "@/stores/blogs.store";
 import { mapState, mapActions } from "pinia";
+import MainBanner from "@/components/MainBanner.vue";
+
 export default {
   name: "BlogsView",
+  components: {
+    MainBanner,
+  },
   data() {
-    return {};
+    return {
+      blogs: [
+        {
+          id: 1,
+          img: "https://placehold.co/900x600",
+          name: "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+          description: "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+          date: "October 6, 2023",
+        },
+        {
+          id: 2,
+          img: "https://placehold.co/900x600",
+          name: "lorem lorem lorem lorem lorem",
+          description: "lorem lorem lorem lorem lorem lorem lorem",
+          date: "October 6, 2023",
+        },
+        {
+          id: 3,
+          img: "https://placehold.co/900x600",
+          name: "lorem lorem lorem lorem lorem",
+          description: "lorem lorem lorem lorem lorem lorem lorem",
+          date: "October 6, 2023",
+        },
+        {
+          id: 4,
+          img: "https://placehold.co/900x600",
+          name: "lorem lorem lorem lorem lorem",
+          description: "lorem lorem lorem lorem lorem lorem lorem",
+          date: "October 6, 2023",
+        },
+        {
+          id: 5,
+          img: "https://placehold.co/900x600",
+          name: "lorem lorem lorem lorem lorem",
+          description: "lorem lorem lorem lorem lorem lorem lorem",
+          date: "October 6, 2023",
+        },
+        {
+          id: 6,
+          img: "https://placehold.co/900x600",
+          name: "lorem lorem lorem lorem lorem",
+          description: "lorem lorem lorem lorem lorem lorem lorem",
+          date: "October 6, 2023",
+        },
+      ],
+    };
   },
   computed: {
     ...mapState(useBlogsStore, ["getBlogs"]),
@@ -27,7 +92,71 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-h1 {
-  font-family: var(--main-font);
+.blog {
+  display: flex;
+  flex-direction: column;
+  background-color: var(--main-color);
+  color: #000;
+  border-radius: 20px;
+  overflow: hidden;
+  border: 1px solid var(--text-color);
+  height: 100%;
+  justify-content: space-between;
+
+  &__img {
+    margin-bottom: 12px;
+  }
+
+  &__title {
+    font-family: var(--secondary-font);
+    font-size: 20px;
+    padding-inline: 16px;
+    color: var(--text-color);
+    cursor: pointer;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
+
+  &__description {
+    font-size: 14px;
+    padding: 16px;
+    margin: 0;
+    color: var(--text-color);
+    cursor: pointer;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 4;
+    line-clamp: 4;
+    -webkit-box-orient: vertical;
+  }
+
+  &__read-more {
+    padding-inline: 16px;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    color: var(--secondary-color);
+    font-size: 12px;
+    text-transform: uppercase;
+    font-weight: 700;
+
+    i {
+      font-size: 8px;
+    }
+  }
+
+  &__footer {
+    padding: 12px 16px;
+    font-weight: 500;
+    border-top: 1px solid #eaeaea;
+    color: var(--secondary-color);
+    font-size: 12px;
+    margin-top: 8px;
+  }
 }
 </style>
