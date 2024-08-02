00911f2c1ee261919362a666d71cd73990a6c17d
Ahmed-B20 <ahmed.banawi123@gmail.com>
2024-08-02 11:59:56 +0300

    working on about page

---
+++ src/components/AboutContent.vue
@@ -0,0 +1,93 @@
+<template>
+    <section class="about-content py-5">
+        <div class="container">
+            <div class="row g-4">
+                <div class="col-12 col-lg-6">
+                    <div class="about-content__box">
+                        <h2 class="about-content__title">
+                            About Us
+                        </h2>
+
+                        <p class="about-content__text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque modi amet atque ut ipsam
+                            voluptatibus quasi, quod accusantium, sit perferendis harum temporibus molestias odit alias
+                            eum
+                            deleniti suscipit nulla recusandae.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque modi amet atque ut ipsam
+                            voluptatibus quasi, quod accusantium, sit perferendis harum temporibus molestias odit alias
+                            eum
+                            deleniti suscipit nulla recusandae.
+                        </p>
+
+                        <p class="about-content__text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque modi amet atque ut ipsam
+                            voluptatibus quasi, quod accusantium, sit perferendis harum temporibus molestias odit alias
+                            eum
+                            deleniti suscipit nulla recusandae.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque modi amet atque ut ipsam
+                            voluptatibus quasi, quod accusantium, sit perferendis harum temporibus molestias odit alias
+                            eum
+                            deleniti suscipit nulla recusandae.
+                        </p>
+
+                        <MainButton text="Courses" link="courses" />
+                    </div>
+                </div>
+
+                <div class="col-12 col-lg-6">
+                    <div class="about-content__box">
+                        <img class="about-content__img" src="https://placehold.co/900x600" alt="about us"
+                            title="about us">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import MainButton from './MainButton.vue';
+
+export default {
+    name: "AboutContent",
+    components: {
+        MainButton
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.about-content {
+    &__box {
+        height: 100%;
+    }
+
+    &__title {
+        font-size: 32px;
+        font-weight: 400;
+        line-height: 1;
+        font-family: var(--secondary-font);
+        color: var(--text-color);
+        margin-bottom: 24px;
+    }
+
+    &__text {
+        font-size: 16px;
+        line-height: 1.5;
+        margin-bottom: 16px;
+        color: var(--text-color);
+
+        &:last-of-type {
+            margin-bottom: 24px;
+        }
+    }
+
+    &__img {
+        width: 100%;
+        height: 100%;
+        border-radius: 24px;
+        border-end-start-radius: 0px;
+        object-fit: cover;
+    }
+}
+</style>
---
+++ src/components/VisionBox.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="vision-box__box">
+        <h2 class="vision-box__title">
+            {{ title }}
+        </h2>
+
+        <p class="vision-box__text">
+            {{ text }}
+        </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "VisionBox",
+    props: {
+
+        title: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.vision-box {
+    &__box {
+        height: 100%;
+    }
+
+    &__title {
+        font-size: 32px;
+        font-weight: 400;
+        line-height: 1;
+        font-family: var(--secondary-font);
+        color: var(--text-color);
+        margin-bottom: 24px;
+    }
+
+    &__text {
+        font-size: 16px;
+        line-height: 1.5;
+        margin-bottom: 0;
+        color: var(--text-color);
+    }
+}
+</style>
