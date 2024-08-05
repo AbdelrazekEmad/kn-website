ecf15f2321cb07e7601497a14305963b3dea6f10
Yousef mostafa <yousef.moustafa@bevatel.com>
2024-08-06 00:41:04 +0300

    footer

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    {{ $t("GLOBAL.FOOTER") }}
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  background-color: var(--blue-color);
+  color: var(--white-color);
+  font-size: 22px;
+  font-size: 500;
+  padding: 8px;
+  text-align: center;
+}
+</style>
