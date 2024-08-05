21e20079aa3ecbd84cd8d345c7049da043bdeb50
Yousef mostafa <yousef.moustafa@bevatel.com>
2024-08-05 04:56:07 +0300

    handle layout & create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
+  <Navbar></Navbar>
   <RouterView />
 </template>
 
+<script>
+  import Navbar from '@/components/layout/Navbar.vue'
+  export default {
+    components: {
+      Navbar,
+    },
+  }
+</script>
+
 <style lang="scss">
-@import "src/assets/scss/main.scss";
+  @import "src/assets/scss/main.scss";
 </style>
