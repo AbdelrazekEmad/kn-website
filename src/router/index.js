33e9a05aaabab0f8ee6392e0756c3a8f6cd6edae
Ahmed-B20 <ahmed.banawi123@gmail.com>
2024-08-24 17:26:00 +0300

    working on fix routes file

--- src/router/index.js
+++ src/router/index.js
@@ -4,77 +4,70 @@ import Tr from "@/i18n/translation";
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_BASE_URL),
   scrollBehavior() {
-    return { top: 0, behavior: 'smooth' }
+    return { top: 0, behavior: "smooth" };
   },
   routes: [
     {
-      path: "/:locale?",
-      component: RouterView,
-      beforeEnter: Tr.routeMiddleware,
+      path: "",
+      redirect: {
+        name: "home-page",
+        params: {
+          locale: Tr.currentLocale,
+        },
+      },
+    },
+    {
+      path: "/:locale?/home",
+      name: "home-page",
+      component: () => import("@/views/HomeView.vue"),
+    },
+    {
+      path: "/:locale?/about-us",
+      name: "about-us",
+      component: () => import("@/views/AboutView.vue"),
+    },
+    {
+      path: "/:locale?/blogs",
+      name: "blogs-page",
+      component: () => import("@/views/BlogsView.vue"),
+    },
+    {
+      path: "/:locale?/blogs/:id",
+      name: "single-blog-page",
+      component: () => import("@/views/SingleBlogView.vue"),
+    },
+    {
+      path: "/:locale?/categories",
+      redirect: { name: "categories-page" },
       children: [
         {
           path: "",
-          redirect: { name: "home-page" }
+          name: "categories-page",
+          component: () => import("@/views/CategoriesView.vue"),
         },
         {
-          path: "home",
-          name: "home-page",
-          component: () => import("@/views/HomeView.vue"),
-        },
-        {
-          path: "about-us",
-          name: "about-us",
-          component: () => import("@/views/AboutView.vue"),
-        },
-        {
-          path: "blogs",
-          name: "blogs-page",
-          component: () => import("@/views/BlogsView.vue"),
-        },
-        {
-          path: "blogs/:id",
-          name: "single-blog-page",
-          component: () => import("@/views/SingleBlogView.vue"),
-        },
-        {
-          path: "categories",
-          redirect: { name: 'categories-page' },
-          children: [
-            {
-              path: "",
-              name: "categories-page",
-              component: () => import("@/views/CategoriesView.vue"),
-            },
-            {
-              path: ":id",
-              props: true,
-              name: "single-categories-page",
-              component: () => import("@/views/CategoryCoursesView.vue"),
-            },
-          ]
-        },
-        {
-          path: "courses/:id",
-          name: "single-course-page",
+          path: ":id",
           props: true,
-          component: () => import("@/views/SingleCourseView.vue"),
-        },
-        {
-          path: "contact-us",
-          name: "contact-us-page",
-          component: () => import("@/views/ContactUsView.vue"),
-        },
-        {
-          path: "not-found",
-          name: "NotFound",
-          component: () => import("@/views/NotFoundView.vue"),
-        },
-        {
-          path: ":pathMatch(.*)*",
-          name: "NotFound",
-          component: () => import("@/views/NotFoundView.vue"),
+          name: "single-categories-page",
+          component: () => import("@/views/CategoryCoursesView.vue"),
         },
       ],
+    },
+    {
+      path: "/:locale?/courses/:id",
+      name: "single-course-page",
+      props: true,
+      component: () => import("@/views/SingleCourseView.vue"),
+    },
+    {
+      path: "/:locale?/contact-us",
+      name: "contact-us-page",
+      component: () => import("@/views/ContactUsView.vue"),
+    },
+    {
+      path: "/:locale?/:pathMatch(.*)*",
+      name: "NotFound",
+      component: () => import("@/views/NotFoundView.vue"),
     },
   ],
 });
